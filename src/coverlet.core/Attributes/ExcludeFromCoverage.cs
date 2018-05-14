9c471d0bf69ea435560a6b4de99568fe6a5f2e20
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-05-14 23:53:50 +0100

    remove all files

--- src/coverlet.core/Attributes/ExcludeFromCoverage.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace Coverlet.Core.Attributes
-{
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method | AttributeTargets.Constructor)]
-    public sealed class ExcludeFromCoverageAttribute : Attribute { }
-}
